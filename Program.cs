e903555f6929489a496fe194a0b4f02e60f54408
Pragati <pragati03verma@hotmail.com>
2025-03-19 22:40:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDoClass toDo = new ToDoClass();
+            bool flag = false;
+            char ch;
+            do
+            {
+                toDo.displayTasks();
+                Console.WriteLine();
+                Console.WriteLine("Select your choice:\n1. Add\n2. Delete\n3. Update\n4. Display\n" +
+                    "5. Quit/Cancel\n");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter a task to do: ");
+                        string task = Console.ReadLine();
+                        toDo.AddTask(task);
+                        break;
+                    case 2:
+                        toDo.displayTasks();
+                        Console.Write("Enter task number which you want to delete: ");
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        toDo.RemoveTask(index);
+                        break;
+                    case 3:
+                        toDo.displayTasks();
+                        Console.Write("Enter task number which you want to update: ");
+                        int taskIndex = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter updated task item: ");
+                        string newTaskItem = Console.ReadLine();
+                        toDo.UpdateTask(taskIndex,newTaskItem);
+                        break;
+                    case 4:
+                        toDo.displayTasks();
+                        break;
+                    case 5:
+                        Console.WriteLine("QUITTING");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid!! Try again!!!");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to try again?? Enter Y to continue. " +
+                    "Enter N to quit.");
+                ch = Convert.ToChar(Console.ReadLine());
+                if(ch == 'Y' ||  ch == 'y')
+                {
+                    flag = true;
+                }
+                else if (ch == 'N' || ch == 'n')
+                {
+                    flag = false;
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option. QUITTING!!!!");
+                    Environment.Exit(0);
+                }
+            } while (flag == true);
+            //Console.WriteLine("QUITTING");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ToDoClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class ToDoClass
+    {
+        private List<string> list = new List<string>()
+        {
+            "Leetcode","Web dev","Angular", "C#"
+        };
+        public void AddTask(string enteredTask)
+        {
+            //list.Add(enteredTask);
+            string file = @"D:\Practice\C#\$$PROJECTS\ToDoList\ListItems.txt";
+            displayTasks();
+        }
+        public void RemoveTask(int index)
+        {
+            list.RemoveAt(index - 1);
+            displayTasks();
+        }
+        public void UpdateTask(int index, string updatedTask)
+        {
+            //list.ElementAt(index);
+            list.RemoveAt(index - 1);
+            list.Insert(index-1, updatedTask);
+            displayTasks();
+        }
+        public void displayTasks()
+        {
+            Console.WriteLine("\nTO DO TASKS: ");
+            string file = @"D:\Practice\C#\$$PROJECTS\ToDoList\ListItems.txt";
+            if(File.Exists(file))
+            {
+                string text = File.ReadAllText(file);
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("File does not exist. Please recreate");
+                Environment.Exit(0);
+            }
+            //int i = 1;
+            //foreach (var task in list)
+            //{
+            //    Console.WriteLine(i+ ". " + task);
+            //    i++;
+            //}
+            Console.WriteLine();
+        }
+        public void writeTasksToFile()
+        {
+            string file = @"D:\Practice\C#\$$PROJECTS\ToDoList\ListItems.txt";
+            string[] tasks = File.ReadAllLines(file);
+            List<string> taskList = new List<string>();
+            taskList = tasks.ToList();
+            foreach (string task in taskList)
+            {
+                Console.WriteLine(task);
+            } 
+        }
+        public void ReadFileItems()
+        {
+            string file = @"D:\Practice\C#\$$PROJECTS\ToDoList\ListItems.txt";
+            string[] tasks = File.ReadAllLines(file);
+            List<string> taskList = new List<string>();
+            taskList = tasks.ToList();
+        }
+    }
+}
